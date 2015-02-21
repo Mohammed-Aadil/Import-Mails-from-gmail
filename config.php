7946a3e0a72b6b6390792b4ad3b463c84523eda5
Mohammed-Aadil <mailtoaadilhanif@gmail.com>
2015-02-21 06:26:13 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+	$clientid	=	'your client id';
+	$clientsecret	=	'your client screte id';
+	$redirecturi	=	'redirect uri same as in developer consol';
+	$maxresults	=	 50;//can be user specified
+?>
